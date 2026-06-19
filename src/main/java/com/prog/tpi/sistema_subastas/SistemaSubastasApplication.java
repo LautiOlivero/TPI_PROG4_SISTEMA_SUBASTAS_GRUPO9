15c395681d8ba76143ca52eb43afc01242501dfc
Lautaro <lautaroolivero44@gmail.com>
2026-06-18 21:07:23 -0300

    Base Del Proyecto

---
+++ src/main/java/com/prog/tpi/sistema_subastas/SistemaSubastasApplication.java
@@ -0,0 +1,13 @@
+package com.prog.tpi.sistema_subastas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSubastasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSubastasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prog/tpi/sistema_subastas/SistemaSubastasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog.tpi.sistema_subastas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSubastasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
