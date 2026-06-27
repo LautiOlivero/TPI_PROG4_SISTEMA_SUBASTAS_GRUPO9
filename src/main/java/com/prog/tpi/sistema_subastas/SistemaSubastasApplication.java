a6504f633f0a214146cb502a03f140aaa30e5ba5
LucianaSassia <sassialuciana@gmail.com>
2026-06-27 01:13:39 -0300

    Fase 2 punto 2 completa: Configuración de Entorno y Base de Datos

--- src/main/java/com/prog/tpi/sistema_subastas/SistemaSubastasApplication.java
+++ src/main/java/com/prog/tpi/sistema_subastas/SistemaSubastasApplication.java
@@ -3,10 +3,13 @@ package com.prog.tpi.sistema_subastas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SistemaSubastasApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure().systemProperties().load();
 		SpringApplication.run(SistemaSubastasApplication.class, args);
 	}
 
