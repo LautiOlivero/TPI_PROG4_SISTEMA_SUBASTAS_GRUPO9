b6cbb52ce373e53fef667893f7da1ba056442c66
LucianaSassia <sassialuciana@gmail.com>
2026-07-01 00:01:46 -0300

    Fix PujaRepository overwriting error

--- src/main/java/com/prog/tpi/sistema_subastas/repositories/PujaRepository.java
+++ src/main/java/com/prog/tpi/sistema_subastas/repositories/PujaRepository.java
@@ -5,6 +5,17 @@ import com.prog.tpi.sistema_subastas.models.Subasta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PujaRepository extends JpaRepository<Puja, Long> {
+
+    List<Puja> findBySubastaOrderByFechaPujaDesc(Subasta subasta);
+
+    Optional<Puja> findTopBySubastaOrderByMontoOfertadoDesc(Subasta subasta);
+
+    boolean existsBySubastaId(Long subastaId);
+
+    List<Puja> findBySubastaIdOrderByFechaPujaDesc(Long subastaId);
 }
